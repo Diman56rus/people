6598d0ee2630cf15619fe9497cb6e82620823315
Dmitry Machavariani <angusmcculloch@yandex.ru>
2015-12-17 09:30:15 +0300

    Обновление с ноуте

---
+++ www/js/app.js
@@ -0,0 +1,162 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.models', 'ngCordova'])
+
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    cordova.plugins.Keyboard.disableScroll(true);
+                }
+                
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
+
+        .constant('Config', {
+            'SITE_URL': 'http://sevgurasp.ru',
+            'API_URL': 'http://sevgurasp.ru/api/schedule.php',
+            'VERSION': 0.31,
+            'DAY_OF_WEEK': [
+                {id: 1, title: "Понедельник"},
+                {id: 2, title: "Вторник"},
+                {id: 3, title: "Среда"},
+                {id: 4, title: "Четверг"},
+                {id: 5, title: "Пятница"},
+                {id: 6, title: "Суббота"}
+            ]
+        })
+
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                    .state('app', {
+                        url: '/app',
+                        abstract: true,
+                        templateUrl: 'templates/menu.html',
+                        controller: 'AppCtrl',
+                        resolve: {
+                            data: function($http, $rootScope, $cordovaNetwork, $ionicPopup, Schedule, Group){
+                                window.localStorage["offline"] = $cordovaNetwork.isOffline();
+                                // window.localStorage["offline"] = "false";
+
+                                if(!window.localStorage["group"]){
+                                    $rootScope.data = {};
+
+                                    Group.getGroups().success(function(data){
+                                        window.localStorage["groups"] = JSON.stringify(data);
+                                        $rootScope.data.groups = JSON.parse(window.localStorage["groups"]);
+                                    });
+
+                                    return $ionicPopup.show({
+                                        templateUrl: 'templates/group-show/group_show.html',
+                                        title: 'Введите свою группу',
+                                        scope: $rootScope,
+                                        buttons: [{
+                                            text: '<b>Схоронить</b>',
+                                            type: 'button-positive',
+                                            onTap: function(e){
+                                                if(!$rootScope.data.group || !$rootScope.data.subgroup){
+                                                    e.preventDefault();
+                                                }else{
+                                                    window.localStorage["group"] = $rootScope.data.group;
+                                                    window.localStorage["subgroup"] = $rootScope.data.subgroup;
+                                                }
+                                            }
+                                        }]
+                                    }).then(function(){
+                                        return Schedule.getAllSchedule().success(function(data){
+                                            window.localStorage["schedule"] = JSON.stringify(data);
+                                        });
+                                    });
+                                }
+
+                                if(!window.localStorage["schedule"] || window.localStorage["offline"] == "false"){
+                                    return Schedule.getAllSchedule().success(function(data){
+                                        window.localStorage["schedule"] = JSON.stringify(data);
+                                    });
+                                }
+                            }
+                        }
+                    })
+
+                    .state('app.main', {
+                        url: '/main',
+                        cache: false,
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/main.html',
+                                controller: 'ScheduleCtrl'
+                            }
+                        }
+                    })
+
+                    .state('app.schedule', {
+                        url: '/schedule',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/schedule.html',
+                                controller: 'ScheduleCtrl',
+                            }
+                        }
+                    })
+
+                    .state('app.day', {
+                        url: '/day/:id',
+                        cache: false,
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/day.html',
+                                controller: 'ScheduleCtrl'
+                            }
+                        }
+                    })
+                    
+                    .state('app.teachers', {
+                        url: '/teachers',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/teachers.html',
+                                controller: 'TeacherCtrl'
+                            }
+                        },
+                        resolve: {
+                            teachers: function($http, Config){
+                                if(window.localStorage["offline"] == "false"){
+                                    var request = "?cmd=getteacher";
+
+                                    return $http.get(Config.API_URL + request).success(function (data) {
+                                        return data;
+                                    });
+                                }
+                            }
+                        }
+                    })
+
+                    .state('app.settings', {
+                        url: '/settings',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/settings.html',
+                                controller: 'SettingsCtrl'
+                            }
+                        }
+                    })
+
+                    .state('app.about', {
+                        url: '/about',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/about.html',
+                                controller: 'AboutCtrl'
+                            }
+                        }
+                    });
+            // if none of the above states are matched, use this as the fallback
+            $urlRouterProvider.otherwise('/app/main');
+        })
---
+++ www/js/controllers.js
@@ -0,0 +1,77 @@
+angular.module("starter.controllers", [])
+        .controller("AppCtrl", function ($scope, $ionicPopup, data) {
+            if (window.localStorage["offline"] == "true" && !window.localStorage["schedule"]){
+                $ionicPopup.alert({
+                    title: "Добро пожаловать!",
+                    template: "Для нормального функционирования приложения нужно подключение к интернету, всего лишь 1 раз :-)"
+                });
+            }
+        })
+
+        .controller("ScheduleCtrl", function ($scope, $state, $interval, Config, Schedule) {
+            $scope.go = function(){
+                window.open(Config.SITE_URL + '?r=phone/index', '_system', 'location=yes');
+            }
+
+            $interval(function(){
+                $scope.diff = function(date_start, date_end){
+                    return Schedule.difference(date_start, date_end);
+                };
+            }, 1000);
+
+            $scope.getDayNum = function(){
+                var num = $state.params;
+                var date = new Date().getDay();
+                num = (typeof num["id"] == "undefined") ? date : num["id"];
+
+                return num;
+            };
+
+            $scope.cur_date = new Date();
+            $scope.dayOfWeek = Config.DAY_OF_WEEK;
+            $scope.msg = Schedule.getSchedule($scope.getDayNum());
+
+            var hour = $scope.cur_date.getHours();
+            var minute = $scope.cur_date.getMinutes();
+            var second = "00";
+
+            $scope.date = (hour < 10 ? "0" + hour : hour) + ":" + (minute < 10 ? "0" + minute : minute) + ":" + second;
+        })
+
+        .controller("TeacherCtrl", function ($scope, $http, teachers) {
+            if(window.localStorage["offline"] == "false")
+                window.localStorage["teachers"] = JSON.stringify(teachers.data);
+
+            $scope.teachers = JSON.parse(window.localStorage["teachers"]);
+        })
+
+        .controller("SettingsCtrl", function ($scope, $ionicPopup, Schedule){
+            $scope.settings = {
+                "group": window.localStorage["group"],
+                "subgroup": window.localStorage["subgroup"]
+            };
+
+            $scope.settings.groups = JSON.parse(window.localStorage["groups"]);
+
+            $scope.savesettings = function(){
+                window.localStorage["group"] = $scope.settings.group;
+                window.localStorage["subgroup"] = $scope.settings.subgroup;
+
+                $ionicPopup.alert({
+                    title: "Успешно!",
+                    template: "Настройки успешно сохранены"
+                }).then(function(){
+                    Schedule.getAllSchedule().success(function(data){
+                        window.localStorage["schedule"] = JSON.stringify(data);
+                    });
+                });
+            };
+
+            $scope.offline = window.localStorage["offline"];
+        })
+
+        .controller("AboutCtrl", function ($scope, Config){
+            $scope.go = function(){
+                window.open(Config.SITE_URL, '_system', 'location=yes');
+            }
+        });
